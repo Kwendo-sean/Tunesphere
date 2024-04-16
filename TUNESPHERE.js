1a0b2c18fcbf180099ae867136c56f62fd1371c5
Kwendo-sean <167208283+Kwendo-sean@users.noreply.github.com>
2024-04-16 21:33:17 +0300

    Add files via upload

---
+++ TUNESPHERE.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // JavaScript code goes here
+    // For example, you can add event listeners or manipulate the DOM
+  });
+  let age = 23;
+ 
+ console.log(`You are ${age} Years old`)
+
+ //window.alert(`Welcome To Tunesphere`)
+let firstname = ('Tunyy')
+console.log(typeof firstname)
+console.log(`Hi,My name is ${firstname}`)
+let available = true;
+console.log(`Is this item available for sale?:${true}`)
